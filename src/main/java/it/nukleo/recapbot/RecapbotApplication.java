4c702580b17814c373462891edbf388c48d2c43a
spidernukleo <denisirmici04@gmail.com>
2026-05-15 20:26:53 +0200

    spring template

---
+++ src/main/java/it/nukleo/recapbot/RecapbotApplication.java
@@ -0,0 +1,13 @@
+package it.nukleo.recapbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapbotApplication.class, args);
+	}
+
+}
